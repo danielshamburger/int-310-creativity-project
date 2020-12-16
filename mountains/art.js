188bcc3433ec6ee031d2965e2d452c966a9b0282
Daniel Shamburger <daniel.shamburger@sparkbusinessworks.com>
2020-12-16 15:30:20 -0500

    add time of day animation

--- mountains/art.js
+++ mountains/art.js
@@ -1,3 +1,14 @@
-var timeOptions = document.querySelectorAll('.time-option');
+const timeOptions = document.querySelectorAll('.time-option');
+const artboard = document.getElementById('artboard');
 
-timeOptions.forEach( radio => radio.addEventListener('change', () => console.log(radio.value) ));
+timeOptions.forEach( radio => radio.addEventListener('change', () => {
+    console.log(radio.value)
+
+    artboard.classList.remove('early-morning');
+    artboard.classList.remove('late-morning');
+    artboard.classList.remove('early-afternoon');
+    artboard.classList.remove('late-afternoon');
+
+    artboard.classList.add(radio.value);
+}));
+
